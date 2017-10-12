e2aa1000bf0b6ff26aec49bdd98d1da31cfda5b8
Nikita Melentev <Nikita.Melentev@kronshtadt.ru>
2017-10-12 21:17:51 +0300

    fix codestyle

--- aiorequests.py
+++ aiorequests.py
@@ -5,36 +5,18 @@ from requests import *
 
 
 __version__ = "0.1.0"
-version = tuple(map(int, str.split(__version__, ".")))
+version = tuple(map(int, __version__.split(".")))
 
 
 def executor_wrapper(f, *, loop=None, executor=None):
-
     loop = loop or asyncio.get_event_loop()
 
     @functools.wraps(f)
-    async def function_with_async_executor(*args, **kwargs):
-
-        return await loop.run_in_executor(
-            executor,
-            functools.partial(
-                f,
-                *args,
-                **kwargs,
-            )
-        )
+    def function_with_async_executor(*args, **kwargs):
+        return loop.run_in_executor(executor, functools.partial(f, *args, **kwargs))
 
     return function_with_async_executor
 
 
 def set_async_requests(*, loop=None, executor=None):
-
-    setattr(
-        Session,
-        "request",
-        executor_wrapper(
-            Session.request,
-            loop=loop,
-            executor=executor,
-        )
-    )
+    setattr(Session, "request", executor_wrapper(Session.request, loop=loop, executor=executor))
